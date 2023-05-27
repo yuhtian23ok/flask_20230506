cad709dcded837a5706ea7749c4108c1d94c949a
yuhtian23ok <61194798+yuhtian23ok@users.noreply.github.com>
2023-05-27 08:26:37 +0000

    modified

---
+++ lesson4-2_0527-streamlit.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+import streamlit as st
+url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+response = requests.request('GET',url)
+if response.status_code == 200:
+    print("連線成功")
+    all_data = response.json()
+    print(type(all_data))
+else:
+    print(f"連線失敗:{response.status_code}")
+
+
+
+dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
+min = int(input("請輸入要查詢的可借數量:"))
+mask = dataFrame['sbi'] <= min
+mask_dataFrame = dataFrame[mask]
+#filename = f'可借小於{min}的站點'
+#mask_dataFrame.to_csv(filename+'.csv')
+#mask_dataFrame.to_excel(filename+'.xlsx')
+st.dataframe(mask_dataFrame)
