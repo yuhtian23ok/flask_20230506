52a4dce4cb6c90c0210d2e47f9ecbfdf2f9946a0
yuhtian23ok <61194798+yuhtian23ok@users.noreply.github.com>
2023-06-10 06:06:43 +0000

    modified

--- lesson5-11-2_20230603-streamlit-sample1 copy.py
+++ lesson5-11-2_20230603-streamlit-sample1 copy.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 import streamlit as st
 
-#下載youbike資料
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
 if response.status_code == 200:
@@ -13,12 +12,12 @@ else:
     print(f"連線失敗:{response.status_code}")
 
 
-#將youbike資料轉成dataFrame
+
 dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
 
 dataFrame.columns = ["站點名稱","車數","可借","行政區","時間","地址","可還","狀態"]
 dataFrame1 = dataFrame.set_index("站點名稱")
-areas = dataFrame1['行政區'].unique()
+#areas = dataFrame1['行政區'].unique()
 
 min,max = st.slider(
     '請選擇可借的(<=數量)',
@@ -28,11 +27,7 @@ mask_dataFrame = dataFrame1[mask]
 count = mask_dataFrame["車數"].count()
 st.write("符合條件的站點數:",count)
 st.dataframe(mask_dataFrame)
+areas = mask_dataFrame['行政區'].unique()
+option = st.selectbox('行政區域',areas)
 
-option = st.selectbox(':accept:行政區域',areas)
-
-mask = dataFrame1['行政區'] == option
-dataFrame2 = dataFrame1[mask]
-
-st.write(option,":",len(dataFrame2.index))
-st.dataframe(dataFrame2)
+st.write('You selected:', option)
